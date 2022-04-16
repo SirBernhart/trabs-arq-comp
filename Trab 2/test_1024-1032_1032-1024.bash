96f12fad00eac9d514e976274e38564e5382ea52
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-04-15 22:33:42 -0300

    Cria novo teste com matriz muito grande e atualiza teste básico para usar o comando -mfma

--- Trab 2/test_1024-1032_1032-1024.bash
+++ Trab 2/test_1024-1032_1032-1024.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -Wall -o matrix_generator matrix_generator.c
-./matrix_generator 8 16 8
+./matrix_generator 1024 1032 1024
 gcc -Wall -mfma -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c
-./matrix_lib_test 1 8 16 16 8 matrix1.bin matrix2.bin result1.bin result2.bin
+./matrix_lib_test 1 1024 1032 1032 1024 matrix1.bin matrix2.bin result1.bin result2.bin
