908bb5462aa8205e3fe236b2a8dc78285720abc1
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-04-15 22:32:03 -0300

    Conserta erro de gravação de matrizes muito grandes

--- Trab 2/matrix_generator.c
+++ Trab 2/matrix_generator.c
@@ -35,7 +35,19 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    fwrite(matrix1, sizeof(float), 128, fp);
+    int matrix1Size = rowsA * colsARowsB;
+    int matrix2Size = colsARowsB * colsB;
+    float* auxMatrix1 = matrix1;
+    float* auxMatrix2 = matrix2;
+
+    for (int i = 0; i < matrix1Size; i += 8, auxMatrix1 += 8)
+    {
+        if (fwrite(auxMatrix1, sizeof(float), 8, fp) != 8)
+        {
+        printf("Error writing to file matrix1.bin: short write (less than 8 floats)\n");
+        return 0;
+        }
+    }
 
     fclose(fp);
 
@@ -46,7 +58,14 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    fwrite(matrix2, sizeof(float), 128, fp);
+    for (int i = 0; i < matrix2Size; i += 8, auxMatrix2 += 8)
+    {
+        if (fwrite(auxMatrix2, sizeof(float), 8, fp) != 8)
+        {
+        printf("Error writing to file matrix2.bin: short write (less than 8 floats)\n");
+        return 0;
+        }
+    }
 
     fclose(fp);
 
