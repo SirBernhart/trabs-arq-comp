b98460764acfb75b2010150a77fb3be7048e985b
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-05-31 12:28:05 -0300

    Atualiza scripts de teste

--- Trab 4/test_1024-1032_1032-1024.bash
+++ Trab 4/test_1024-1032_1032-1024.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -Wall -o matrix_generator matrix_generator.c
 ./matrix_generator 1024 1032 1024
-gcc -Wall -mfma -o matrix_lib_test matrix_lib_test.c matrix_lib.c timer.c
-./matrix_lib_test 1 1024 1032 1032 1024 matrix1.bin matrix2.bin result1.bin result2.bin
+nvcc -o matrix_lib_test matrix_lib_test.cu matrix_lib.cu timer.c
+./matrix_lib_test 2 1024 1032 1032 1024 256 4096 matrix1.bin matrix2.bin result1.bin result2.bin
--- Trab 4/test_8-16_16-8.bash
+++ Trab 4/test_8-16_16-8.bash
@@ -2,4 +2,4 @@
 gcc -Wall -o matrix_generator matrix_generator.c
 ./matrix_generator 8 16 8
 nvcc -o matrix_lib_test matrix_lib_test.cu matrix_lib.cu timer.c
-./matrix_lib_test 2 8 16 16 8 matrix1.bin matrix2.bin result1.bin result2.bin
+./matrix_lib_test 2 8 16 16 8 256 4096 matrix1.bin matrix2.bin result1.bin result2.bin
