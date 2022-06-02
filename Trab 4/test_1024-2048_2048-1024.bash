b2616bc7a35fd9c8a923b467753b2981d2891ee9
Bernardo Hörner <bernardo.horner@tapps.com.br>
2022-06-02 12:51:37 -0300

    Teste para matrizes de 1024x2048 e 2048x1024

---
+++ Trab 4/test_1024-2048_2048-1024.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+gcc -Wall -o matrix_generator matrix_generator.c
+./matrix_generator 1024 2048 1024
+nvcc -o matrix_lib_test matrix_lib_test.cu matrix_lib.cu timer.c
+./matrix_lib_test 2 1024 2048 2048 1024 256 4096 matrix1.bin matrix2.bin result1.bin result2.bin
